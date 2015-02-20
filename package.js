0fe96145694b7b4613066b8f9712d850aeb7687a
dmorri <davide.morri@mondora.com>
2015-02-20 15:44:44 +0100

    incremented version, now 0.1.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "mondora:versioned-collection",
     summary: "Versioned collections for meteor",
-    version: "0.1.4",
+    version: "0.1.5",
     git: "https://github.com/mondora/mondora-versioned-collection.git"
 });
 
