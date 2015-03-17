2d75b2b842202257707ef32ba3fe26d4cf4f2ba0
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-03-17 17:32:55 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "mondora:versioned-collection",
     summary: "Versioned collections for meteor",
-    version: "0.2.0",
+    version: "0.2.1",
     git: "https://github.com/mondora/mondora-versioned-collection.git"
 });
 
